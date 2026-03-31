66ea647f06ac860d458f611e76f9e0952e4f8f99
anass <anass.mokhless123@gmail.com>
2026-03-31 13:45:03 +0000

    mongodbstart

--- HighscorerHavenV2/index.ts
+++ HighscorerHavenV2/index.ts
@@ -5,6 +5,7 @@ import { MongoClient } from "mongodb";
 import { searchPageRouter } from "./routers/searchpage";
 import { detailPageRouter } from "./routers/detail";
 import { libraryRouter } from "./routers/library";
+import { start } from "repl";
 
 dotenv.config();
 
@@ -15,6 +16,11 @@ export const client = new MongoClient(uri);
 const gamesQuery = client.db("highscorehaven").collection("games");
 const usersQuery = client.db("highscorehaven").collection("users");
 
+
+const uri = "mongodb+srv://havenhighscore_db_user:haven@highscorehaven.tjuwhvt.mongodb.net/?appName=Highscorehaven"
+const client = new MongoClient(uri);
+const gamesQuery = client.db("highscorehaven").collection("games");
+const usersQuery = client.db("highscorehaven").collection("users");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -43,3 +49,11 @@ app.get("/battle", (req, res) => {
 app.listen(app.get("port"), () => {
   console.log("Server started on http://localhost:" + app.get("port"));
 });
+
+app.listen(app.get("port"), () => {
+    console.log("Server started on http://localhost:" + app.get('port'));
+});
+
+async function main(){
+    
+}
