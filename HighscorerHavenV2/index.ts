93a03c81fbb6d8ebb3cdf9618fe574ec7a95e66f
Leo Lam <Leolam20021@gmail.com>
2026-03-31 15:15:51 +0000

    push

--- HighscorerHavenV2/index.ts
+++ HighscorerHavenV2/index.ts
@@ -8,11 +8,14 @@ import bcrypt from "bcrypt";
 dotenv.config();
 
 const app: Express = express();
+<<<<<<< HEAD
 
 const uri = "mongodb+srv://havenhighscore_db_user:haven@highscorehaven.tjuwhvt.mongodb.net/?appName=Highscorehaven";
 const client = new MongoClient(uri);
 const gamesQuery = client.db("HighscoreHaven").collection("Games");
 const usersQuery = client.db("HighscoreHaven").collection("Users");
+=======
+>>>>>>> a92cd19 (.)
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -27,8 +30,16 @@ app.get("/", (req, res) => {
     title: "Hello World",
     message: "Hello World",
   });
+<<<<<<< HEAD
+=======
 });
-
+app.get("/battle", (req, res) => {
+  res.render("index", {
+    title: "Hello World",
+    message: "Hello World",
+  });
+>>>>>>> a92cd19 (.)
+});
 app.listen(app.get("port"), () => {
   console.log("Server started on http://localhost:" + app.get("port"));
 });
