1ffaf5f7a3a56b35c724a3d122086b3475161a4e
Michaël Luyten <ik.benik@outlook.com>
2026-03-31 14:18:50 +0000

    toevoeging start router searchpage

--- HighscorerHavenV2/index.ts
+++ HighscorerHavenV2/index.ts
@@ -1,26 +1,29 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { searchpagerouter } from "routers/searchpage";
 
 dotenv.config();
 
-const app : Express = express();
+const app: Express = express();
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
 
+app.use("/searchpage", searchpagerouter(searchPage));
+
 app.get("/", (req, res) => {
-    res.render("index", {
-        title: "Hello World",
-        message: "Hello World"
-    })
+  res.render("index", {
+    title: "Hello World",
+    message: "Hello World",
+  });
 });
 
 app.listen(app.get("port"), () => {
-    console.log("Server started on http://localhost:" + app.get('port'));
+  console.log("Server started on http://localhost:" + app.get("port"));
 });
