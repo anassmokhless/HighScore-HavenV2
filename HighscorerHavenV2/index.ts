11eb749050b1a9008e2ee8995a9768ec75248ce2
Leo Lam <Leolam20021@gmail.com>
2026-04-03 13:02:32 +0000

    sessionUser

--- HighscorerHavenV2/index.ts
+++ HighscorerHavenV2/index.ts
@@ -1,10 +1,10 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import session from "express-session";
 
-//routers
+// Routers
 import registerRouter from "./routers/registeren";
 import loginRouter from "./routers/login";
 import compareRouter from "./routers/compare";
@@ -27,6 +27,7 @@ const uri =
 
 const uri =
   "mongodb+srv://havenhighscore_db_user:haven@highscorehaven.tjuwhvt.mongodb.net/?appName=Highscorehaven";
+<<<<<<< HEAD
 
 >>>>>>> 74a50ab (compair not done)
 const client = new MongoClient(uri);
@@ -35,19 +36,20 @@ const usersQuery = client.db("highscorehaven").collection("users");
 >>>>>>> 18b96e3 (login en registeren (express-session enbcrypt))
 =======
 
+=======
+>>>>>>> 0fce08b (sessionUser)
 const client = new MongoClient(uri);
 export const gamesQuery = client.db("HighscoreHaven").collection("Games");
 export const usersQuery = client.db("HighscoreHaven").collection("Users");
 >>>>>>> 3dfcda9 (update session en compair)
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("port", process.env.PORT || 3000);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
-app.set("views", path.join(__dirname, "views"));
-
-app.set("port", process.env.PORT || 3000);
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -66,6 +68,7 @@ app.use(
   }),
 );
 
+// Routers
 app.use(registerRouter);
 app.use(loginRouter);
 app.use(compareRouter);
@@ -74,19 +77,33 @@ app.use(compareRouter);
 =======
 >>>>>>> 3dfcda9 (update session en compair)
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Hello World",
-    message: "Hello World",
-  });
+// Pagina's
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
+<<<<<<< HEAD
 app.get("/battle", (req, res) => {
   res.render("index", {
     title: "Hello World",
     message: "Hello World",
   });
 <<<<<<< HEAD
+=======
+app.get("/startpage", async (req, res) => {
+  if (!(req.session as any).user) {
+    return res.redirect("/login");
+  }
+
+  const sessionUser = (req.session as any).user;
+  const user = await usersQuery.findOne({ _id: new ObjectId(sessionUser.id) });
+
+  if (!user) {
+    return res.redirect("/login");
+  }
+
+  res.render("startpage", { user });
+>>>>>>> 0fce08b (sessionUser)
 });
 <<<<<<< HEAD
 app.get("/battle", (req, res) => {
@@ -110,8 +127,11 @@ app.use(loginRouter);
 >>>>>>> 18b96e3 (login en registeren (express-session enbcrypt))
 
 app.listen(app.get("port"), () => {
-  console.log("Server started on http://localhost:" + app.get("port"));
+  console.log(
+    "Server started on http://localhost:" + app.get("port") + "/login",
+  );
 });
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 });
@@ -134,3 +154,5 @@ async function main(){
 =======
 async function main() {}
 >>>>>>> 3dfcda9 (update session en compair)
+=======
+>>>>>>> 0fce08b (sessionUser)
