726708607a9c6dc397d0794a3bdf2109b80f0e8d
Leo Lam <Leolam20021@gmail.com>
2026-04-04 11:38:43 +0000

    v1 battle en compare

--- HighscorerHavenV2/routers/compare.ts
+++ HighscorerHavenV2/routers/compare.ts
@@ -3,7 +3,7 @@ import { gamesQuery as gamesCollection } from "../index";
 
 const router = express.Router();
 
-
+// eerst inloggen
 router.get("/compare", (req, res) => {
   if (!(req.session as any).user) {
     return res.redirect("/login");
