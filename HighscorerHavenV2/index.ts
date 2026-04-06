5ae689392c742dad431634b0f925e25c8ddb031a
Michaël Luyten <ik.benik@outlook.com>
2026-04-06 08:57:19 +0000

    mini fix na merge

--- HighscorerHavenV2/index.ts
+++ HighscorerHavenV2/index.ts
@@ -19,10 +19,9 @@ const app: Express = express();
 const uri =
   "mongodb+srv://havenhighscore_db_user:haven@highscorehaven.tjuwhvt.mongodb.net/?appName=Highscorehaven";
 
-const client = new MongoClient(uri);
+export const client = new MongoClient(uri);
 export const gamesQuery = client.db("HighscoreHaven").collection("Games");
 export const usersQuery = client.db("HighscoreHaven").collection("Users");
-
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -31,7 +30,6 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.use(
   session({
@@ -51,7 +49,6 @@ app.use("/searchpage", searchPageRouter());
 app.use("/detail", detailPageRouter());
 app.use("/library", libraryRouter());
 
-
 // Pagina's
 app.get("/", (req, res) => {
   res.render("index");
@@ -60,62 +57,57 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-
 app.get("/battle", (req, res) => {
   res.render("index", {
     title: "Hello World",
     message: "Hello World",
   });
 
-app.get("/startpage", async (req, res) => {
-  if (!(req.session as any).user) {
-    return res.redirect("/login");
-  }
+  app.get("/startpage", async (req, res) => {
+    if (!(req.session as any).user) {
+      return res.redirect("/login");
+    }
 
-  const sessionUser = (req.session as any).user;
-  const user = await usersQuery.findOne({ _id: new ObjectId(sessionUser.id) });
+    const sessionUser = (req.session as any).user;
+    const user = await usersQuery.findOne({
+      _id: new ObjectId(sessionUser.id),
+    });
 
-  if (!user) {
-    return res.redirect("/login");
-  }
+    if (!user) {
+      return res.redirect("/login");
+    }
 
-  res.render("startpage", { user });
+    res.render("startpage", { user });
+  });
+  app.get("/battle", (req, res) => {
+    res.render("index", {
+      title: "Hello World",
+      message: "Hello World",
+    });
+  });
 
-});
-app.get("/battle", (req, res) => {
-  res.render("index", {
-    title: "Hello World",
-    message: "Hello World",
+  app.use(
+    session({
+      secret: "jouw-geheime-sleutel",
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 dag
+    }),
+  );
+  app.use(registerRouter);
+  app.use(loginRouter);
+
+  app.listen(app.get("port"), () => {
+    console.log("Server started on http://localhost:" + app.get("port"));
   });
 });
 
-app.use(
-  session({
-    secret: "jouw-geheime-sleutel",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 dag
-  }),
-);
-app.use(registerRouter);
-app.use(loginRouter);
-
 app.listen(app.get("port"), () => {
   console.log("Server started on http://localhost:" + app.get("port"));
 });
 
-});
-
 app.listen(app.get("port"), () => {
   console.log("Server started on http://localhost:" + app.get("port"));
-
 });
-
-app.listen(app.get("port"), () => {
-  console.log("Server started on http://localhost:" + app.get("port"));
-
-});
-
-
 
 async function main() {}
