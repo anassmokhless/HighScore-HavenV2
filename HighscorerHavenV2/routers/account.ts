c32ed8ce8c05376e758e59b363be6f6d4f427221
anass <anass.mokhless123@gmail.com>
2026-04-08 16:53:42 +0000

    edit account page added

--- HighscorerHavenV2/routers/account.ts
+++ HighscorerHavenV2/routers/account.ts
@@ -61,5 +61,84 @@ export function accountrouter() {
   });
 
   router.post("/logout", stopSession);
+
+  router.get("/edit", requireAuth, async (req, res) => {
+    try {
+      const sessionUser: LoggedInUser = (req.session as any)
+        .user as LoggedInUser;
+      const user: User | null = await userCollection.findOne<User>({
+        _id: new ObjectId(sessionUser.id),
+      });
+
+      if (!user) {
+        res.redirect("/login");
+        return;
+      }
+
+      res.render("editAccount", {
+        user: user,
+        error: null,
+      });
+    } catch (e) {
+      console.error(e);
+      res.redirect("/account");
+    }
+  });
+
+  router.post("/edit", requireAuth, async (req, res) => {
+    try {
+      const sessionUser: LoggedInUser = (req.session as any)
+        .user as LoggedInUser;
+      const name: string = req.body.name;
+      const username: string = req.body.username;
+      const email: string = req.body.email;
+      const userId: ObjectId = new ObjectId(sessionUser.id);
+      const user: User | null = await userCollection.findOne<User>({
+        _id: userId,
+      });
+
+      if (!user) {
+        res.redirect("/login");
+      } else {
+        let error: string | null = null;
+
+        if (
+          await userCollection.findOne({
+            username: username,
+            _id: { $ne: userId },
+          })
+        ) {
+          error = "Gebruikersnaam is al in gebruik.";
+        } else if (
+          await userCollection.findOne({ email: email, _id: { $ne: userId } })
+        ) {
+          error = "E-mailadres is al in gebruik.";
+        }
+
+        if (error) {
+          res.render("editAccount", {
+            user: user,
+            error: error,
+          });
+        } else {
+          await userCollection.updateOne(
+            { _id: userId },
+            {
+              $set: {
+                name: name.trim(),
+                username: username.trim(),
+                email: email.trim(),
+              },
+            },
+          );
+          (req.session as any).user.username = username.trim();
+          res.redirect("/account");
+        }
+      }
+    } catch (e) {
+      console.error(e);
+      res.redirect("/account");
+    }
+  });
   return router;
 }
