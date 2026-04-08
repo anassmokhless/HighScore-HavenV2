3c1174cd4bfe286362927fae9709bab92ab91b80
anass <anass.mokhless123@gmail.com>
2026-04-08 14:27:15 +0000

    secure env

---
+++ HighscorerHavenV2/routers/account.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { userCollection } from "../database";
+
+export function accountrouter(){
+    const router = express.Router();
+
+    router.get("/", async (req, res) => {
+
+    });
+}
